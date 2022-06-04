f8da83179e4323fe878c5163e658263211066ff2
christopher coyne <christcoyne@gmail.com>
2022-06-03 21:14:36 -0400

    more checking on return value

--- src/screens/Chatbot.js
+++ src/screens/Chatbot.js
@@ -17,9 +17,13 @@ const Chatbot = ({setUserLogs, userLogs, botLogs, setBotLogs, metadata, setMetad
     axios.post('https://n9i31tpdha.execute-api.us-east-1.amazonaws.com/v1/chatbot', newMessage).then(value => {
         console.log('value returned : ', value)
 
+        // 'mlength': {'value': {'originalValue': 'abc', 'resolvedValues': []}}
         const parsed = JSON.parse(value.data.body)
         for (const slot in parsed.sessionState.intent.slots) {
             if (parsed.sessionState.intent.slots[slot] == null) {
+                parsed.sessionState.intent.slots[slot] = {}
+            }
+            if (parsed.sessionState.intent.slots[slot].value && !parsed.sessionState.intent.slots[slot].value.interpretedValue) {
                 parsed.sessionState.intent.slots[slot] = {}
             }
         }
@@ -31,9 +35,11 @@ const Chatbot = ({setUserLogs, userLogs, botLogs, setBotLogs, metadata, setMetad
         // if we have fulfilled getrecs (or anything...), then reset 
         if (parsed.sessionState.intent.state === 'Fulfilled') {
             console.log('fulfilled! reset')
-            setMetadata({...metadata, sessionState: {'sessionState': {'intent': {'name': 'FallbackIntent', slots: {}}}, 'sessionAttributes': {'movies': 'mymovie'}}, sessionStarted: true})
+            setMetadata({...metadata, sessionState: {'intent': {'name': 'FallbackIntent', 'slots': {}}, 'sessionAttributes':parsed.sessionState.sessionAttributes}, sessionStarted: true})
         }
-        setMetadata({...metadata, sessionState: parsed.sessionState, sessionStarted: true})
+        else {
+            setMetadata({...metadata, sessionState: parsed.sessionState, sessionStarted: true})
+        }
     })
 
     setUserLogs([...userLogs, {'content': userInput}])
