4f4453a82d502809da129aa87370454b12b5c192
christopher coyne <christcoyne@gmail.com>
2022-06-07 01:45:49 -0400

    added tailwind css, linted

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Logs = ({messages}) => {
-    if (!messages) {
-        return <></>
-    }
+const Logs = ({ messages }) => {
+  if (!messages) {
+    return <></>;
+  }
   return (
-      <div>
-    {messages.map((msg, ind) => <div key={ind}>{msg.content}</div>)}
+    <div>
+      {messages.map((msg, ind) => (
+        <div key={ind} className="text-3xl font-oxygen">
+          {msg.content}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Logs
+export default Logs;
