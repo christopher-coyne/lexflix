256ef73501e8ba9440c928b564ad381a4d3d5bbe
christopher coyne <christcoyne@gmail.com>
2022-06-06 23:48:37 -0400

    created tailwind config, colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      pink: "#E40066",
+      purple: "#161936",
+      yellow: "#EAC435",
+      fontLightBlue: "#B6C5E2",
+      fontDarkBlue: "#2B4579",
+      lightPink: "#FF9CC8",
+      searchBlue: "#20345A",
+      white: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
