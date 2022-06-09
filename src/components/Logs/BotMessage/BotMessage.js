1b8a85b995f21128a3da7ee2d8fe11993b87e147
christopher coyne <christcoyne@gmail.com>
2022-06-09 01:13:09 -0400

    bot and user message components created

--- src/components/Logs/BotMessage/BotMessage.js
+++ src/components/Logs/BotMessage/BotMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const BotMessage = ({ message }) => {
   return (
-    <div className="container text-left">
+    <div className="container text-left font-oxygen">
       <p className="bg-messageDarkBlue text-fontLightBlue px-4 py-3 max-w-[50%] rounded-bl-2xl rounded-tr-2xl rounded-br-2xl rounded-tl-sm ml-auto mr-0 inline-block text-left">
         {message}
       </p>
--- src/components/Logs/UserMessage/UserMessage.js
+++ src/components/Logs/UserMessage/UserMessage.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const UserMessage = ({ message }) => {
   return (
-    <div className="container text-right mt-2">
-      <p className="bg-pink text-white px-4 py-3 max-w-[50%] rounded-bl-2xl rounded-tl-2xl rounded-br-2xl rounded-tr-sm ml-auto mr-0 inline-block text-left">
+    <div className="container text-right mt-2 font-oxygen">
+      <p className="bg-pink text-white px-4 py-3 max-w-[50%] rounded-bl-2xl rounded-tl-2xl rounded-br-2xl rounded-tr-sm ml-auto mr-0 inline-block text-left transition-all">
         {message}
       </p>
     </div>
