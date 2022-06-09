8f3b582dd26625479689848c274c36c51472262d
christopher coyne <christcoyne@gmail.com>
2022-06-09 01:11:52 -0400

    form updated

--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -4,25 +4,25 @@ import Suggestions from "../../components/Suggestions/Suggestions";
 
 const Submit = ({ userInput, updateInput, submitHandler }) => {
   return (
-    <>
+    <div className="border-4 border-yellow w-[90%] container">
       <Suggestions />
       <form
         onSubmit={(e) => submitHandler(e)}
         autoComplete="off"
-        className="absolute bottom-0 right-[30%]"
+        className=" w-[100%] h-12 border-4 border-white"
       >
         <input
           type="text"
           name="input"
           value={userInput}
           onChange={(e) => updateInput(e)}
-          className="bg-inputBlue text-fontLightBlue p-3 rounded-md"
+          className="bg-inputBlue text-fontLightBlue h-full rounded-md w-[90%] border-none"
         />
-        <button className="bg-inputBlue p-3 rounded-md m-0">
+        <button className="bg-inputBlue rounded-md h-full m-0 w-10 text-center">
           <img src={Triangle} alt="send" />
         </button>
       </form>
-    </>
+    </div>
   );
 };
 
