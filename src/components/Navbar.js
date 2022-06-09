0b8eff6ffafd7db4e3fadb7b5202e5e7754242c4
christopher coyne <christcoyne@gmail.com>
2022-06-09 01:14:02 -0400

    fonts added and styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="container bg-midnightPurple max-w-none flex justify-between py-1.5 px-20 text-2xl text-white oxygen">
+    <header className="container bg-midnightPurple max-w-none flex justify-between py-1.5 px-20 text-2xl text-white">
       <Link to={"/"}>
-        <span className="text-pink">Lex</span>Flix
+        <span className="text-pink font-ubuntu">Lex</span>Flix
       </Link>
-      <Link to={"/about"}>About</Link>
+      <Link to={"/about"} className="font-oxygen">
+        About
+      </Link>
     </header>
   );
 };
