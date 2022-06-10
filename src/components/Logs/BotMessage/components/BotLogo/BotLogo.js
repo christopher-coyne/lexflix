9a0eae7318752789617e934ff0b04ee2fe1c9df8
christopher coyne <christcoyne@gmail.com>
2022-06-10 17:33:05 -0400

    bot logo

---
+++ src/components/Logs/BotMessage/components/BotLogo/BotLogo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BotLogo = () => {
+  return (
+    <h2 className="text-yellow font-lobster absolute -left-7 -top-5 border-yellow border bg-midnightPurple rounded-full h-10 w-10 py-1 pr-1 z-[1] text-center ">
+      L
+    </h2>
+  );
+};
+
+export default BotLogo;
