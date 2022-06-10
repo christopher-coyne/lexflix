fa01d30d99a96032d6f368666d659255ceafb741
christopher coyne <christcoyne@gmail.com>
2022-06-09 20:51:45 -0400

    introcard now responsive

--- src/components/IntroCard/IntroCard.js
+++ src/components/IntroCard/IntroCard.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const IntroCard = () => {
   return (
-    <div className="container justify-between flex flex-row bg-gradient-to-r from-pink to-gradientEndPink mx-auto text-white rounded-2xl w-full mb-8 px-2 py-5 gap-2 font-ubuntu">
-      <h1 className="border-4 border-white rounded-tr-2xl rounded-bl-2xl m-auto text-5xl inline-block px-8 py-4">
+    <div className="container justify-between items-center flex lg:flex-row bg-gradient-to-r from-pink to-gradientEndPink text-white rounded-2xl w-full mb-8 px-4 py-5 gap-4 xl:gap-14 font-ubuntu flex-col">
+      <div className="border-4 border-white rounded-tr-2xl rounded-bl-2xl text-5xl inline-block px-8 mx-auto xl:py-4 lg:py-8 py-4 w-[100%] lg:w-[40%] text-center">
         LexFlix
-      </h1>
-      <div className="container w-[60%] mx-auto text-xl">
+      </div>
+      <div className="container mx-auto text-xl">
         <p>
           A movie chatbot powered by Amazon Lex. The data is taken from IMDB's
           list of top 1000 movies of all time.
