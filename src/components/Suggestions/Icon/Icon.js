6ee3bf474b9ea0f128188a5c3170fb3d8cf91700
christopher coyne <christcoyne@gmail.com>
2022-06-10 17:32:21 -0400

    added lex icon

--- src/components/Suggestions/Icon/Icon.js
+++ src/components/Suggestions/Icon/Icon.js
@@ -4,24 +4,26 @@ const Icon = ({ text, submit, showExtension }) => {
   if (text === "-" || text === "+") {
     return (
       <button
-        className="text-lightPink bg-pink rounded-full h-10 w-10 font-oxygen text-2xl font-bold my-2"
+        className="text-lightPink bg-pink rounded-full h-10 w-10 font-oxygen text-2xl font-bold my-2 hover:shadow-lg hover:shadow-pink/100 hover:text-white"
         onClick={submit}
       >
         {showExtension ? "-" : "+"}
       </button>
     );
   }
-  const color = text === "Don't Care" ? "yellow" : "pink";
+  const bgcolor = text === "Don't Care" ? "bg-yellow" : "bg-pink";
+  const textcolor = text === "Don't Care" ? "text-yellow" : "text-pink";
+  const bordercolor = text === "Don't Care" ? "border-yellow" : "border-pink";
   return (
     <button
       onClick={() => {
         submit(null, text);
       }}
-      className={`${text === "Don't Care" ? "border-yellow" : "border-pink"} ${
-        text === "Don't Care" ? "text-yellow" : "text-pink"
-      }  relative group border font-oxygen px-5 py-2 rounded-md mr-4 my-2 overflow-hidden`}
+      className={`${textcolor} ${bordercolor}  relative group border font-oxygen px-5 py-2 rounded-md mr-4 my-2 overflow-hidden`}
     >
-      <span className="absolute group-hover:w-full transition-all ease-out duration-300 w-0 h-full bg-pink left-0 top-0"></span>
+      <span
+        className={`${bgcolor} absolute group-hover:w-full transition-all ease-out duration-300 w-0 h-full left-0 top-0`}
+      ></span>
       <span className="relative group-hover:text-white">{text}</span>
     </button>
   );
