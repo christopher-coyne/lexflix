f83c96343423dc5c6b70b1fd3c30ad72db3d928f
christopher coyne <christcoyne@gmail.com>
2022-06-10 13:18:55 -0400

    basic button animation

--- src/components/Suggestions/Icon/Icon.js
+++ src/components/Suggestions/Icon/Icon.js
@@ -4,27 +4,29 @@ const Icon = ({ text, submit, showExtension }) => {
   if (text === "-" || text === "+") {
     return (
       <button
-        className="text-lightPink bg-pink rounded-full h-10 w-10 font-oxygen text-2xl font-bold"
+        className="text-lightPink bg-pink rounded-full h-10 w-10 font-oxygen text-2xl font-bold my-2"
         onClick={submit}
       >
         {showExtension ? "-" : "+"}
       </button>
     );
   }
+  const color = text === "Don't Care" ? "yellow" : "pink";
   return (
     <button
       onClick={() => {
         submit(null, text);
       }}
-      className={`${
-        text === "Don't Care" ? "border-yellow" : "border-pink"
-      } border-2 ${
+      className={`${text === "Don't Care" ? "border-yellow" : "border-pink"} ${
         text === "Don't Care" ? "text-yellow" : "text-pink"
-      } font-ubuntu px-5 py-1 rounded-md mr-4`}
+      }  relative group border font-oxygen px-5 py-2 rounded-md mr-4 my-2 overflow-hidden`}
     >
-      {text}
+      <span className="absolute group-hover:w-full transition-all ease-out duration-300 w-0 h-full bg-pink left-0 top-0"></span>
+      <span className="relative group-hover:text-white">{text}</span>
     </button>
   );
 };
 
 export default Icon;
+
+/* border font-oxygen px-5 py-2 rounded-md mr-4 my-2 */
