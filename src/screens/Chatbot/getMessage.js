4f20444a57620fc14e3d76f3a456365d7f41dabe
christopher coyne <christcoyne@gmail.com>
2022-06-11 00:17:39 -0400

    get message now adds card results to state

--- src/screens/Chatbot/getMessage.js
+++ src/screens/Chatbot/getMessage.js
@@ -17,6 +17,8 @@ const getMessage = (
       console.log("value returned : ", value);
 
       // 'mlength': {'value': {'originalValue': 'abc', 'resolvedValues': []}}
+
+      /* clean data. json may have "none" instead of a {} */
       const parsed = JSON.parse(value.data.body);
       for (const slot in parsed.sessionState.intent.slots) {
         if (parsed.sessionState.intent.slots[slot] == null) {
@@ -38,14 +40,32 @@ const getMessage = (
       );
 
       //setMessages([...messages, { content: value.data.body, type: "bot" }]);
+
+      /* get the last message in state with a ... (loading), create a new array,
+       *
+       */
+      const getrecsFinished =
+        parsed.sessionState.intent.state === "Fulfilled" &&
+        parsed.sessionState.intent.name === "getrecs";
       const index = messages.findIndex((msg) => {
         return msg.content === "..." && msg.type === "bot";
       });
       const msgCopy = [...messages];
       msgCopy[index] = {
         ...messages[index],
-        content: parsed.messages.content,
+        content: !getrecsFinished
+          ? parsed.messages.content
+          : JSON.parse(parsed.messages.content)[0],
+        card: getrecsFinished ? true : false,
       };
+
+      if (getrecsFinished) {
+        msgCopy.push({
+          content: JSON.parse(parsed.messages.content)[1],
+          card: true,
+        });
+      }
+      console.log("setting new messages... ", msgCopy);
       setMessages(msgCopy);
 
       // if we have fulfilled getrecs (or anything...), then reset
