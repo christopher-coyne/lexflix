45ea1a9a1c4a2dbed22ff8f252aa2c2c8aa79a00
christopher coyne <christcoyne@gmail.com>
2022-06-10 22:32:36 -0400

    minor styling updates

--- src/components/IntroCard/IntroCard.js
+++ src/components/IntroCard/IntroCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IntroCard = () => {
   return (
-    <div className="container justify-between items-center flex lg:flex-row bg-gradient-to-r from-pink to-gradientEndPink text-white rounded-2xl w-full mb-8 px-4 py-5 gap-4 xl:gap-14 font-ubuntu flex-col">
+    <div className="justify-between items-center flex lg:flex-row bg-gradient-to-r from-pink to-gradientEndPink text-white rounded-2xl w-full mb-8 px-4 py-5 gap-4 xl:gap-14 font-ubuntu flex-col">
       <div className="border-4 border-white rounded-tr-2xl rounded-bl-2xl text-5xl inline-block px-8 mx-auto xl:py-4 lg:py-8 py-4 w-[100%] lg:w-[40%] text-center">
         LexFlix
       </div>
--- src/components/Suggestions/Extension/Extension.js
+++ src/components/Suggestions/Extension/Extension.js
@@ -3,7 +3,7 @@ import Icon from "../Icon/Icon";
 
 const Extension = ({ currentExtension, submit }) => {
   return (
-    <div className="h-[500] border-2 border-pink absolute bottom-[10%] right-[30%] bg-yellow">
+    <div className="h-[500] w-[40%] border-2 border-pink absolute bottom-[10%] right-[27%] bg-midnightPurple text-left p-4 rounded-lg">
       {currentExtension.map((sug) => (
         <Icon submit={submit} text={sug} key={sug} />
       ))}
