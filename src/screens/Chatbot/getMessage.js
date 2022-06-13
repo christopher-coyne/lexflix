7977619a8f2299bedb530a31061ddbcb9ea2f4dc
christopher coyne <christcoyne@gmail.com>
2022-06-13 17:17:33 -0400

    get message handles getmoviesbyperson

--- src/screens/Chatbot/getMessage.js
+++ src/screens/Chatbot/getMessage.js
@@ -1,5 +1,42 @@
-import React from "react";
 import axios from "axios";
+
+const newContentDict = {
+  recs: "Sorry, but we did not find any movie results that matched your query",
+  director: "Sorry, but we did not find any directors with that name",
+  actor: "Sorry, but we did not find any directors with that name",
+};
+
+const getrecsSetMessages = (index, msgCopy, parsed, messages, type) => {
+  let newContent = newContentDict[type];
+  let cards = [];
+  let isCard = false;
+
+  /* change from default values is we get any results */
+  cards = JSON.parse(parsed.messages.content);
+  if (cards.length >= 1) {
+    newContent =
+      "Here are your results! Click on any one of them in order to expand or collapse";
+    isCard = false;
+  }
+  msgCopy[index] = {
+    ...messages[index],
+    content: newContent,
+    card: isCard,
+  };
+
+  for (const card of cards) {
+    msgCopy.push({
+      content: card,
+      card: type,
+    });
+  }
+
+  msgCopy.push({
+    content: "Is there anything else you would like to do?",
+    card: false,
+  });
+  return msgCopy;
+};
 
 const getMessage = (
   newMessage,
@@ -44,14 +81,34 @@ const getMessage = (
       /* get the last message in state with a ... (loading), create a new array,
        *
        */
-      const getrecsFinished =
+      const intentFinished =
         parsed.sessionState.intent.state === "Fulfilled" &&
-        parsed.sessionState.intent.name === "getrecs";
+        (parsed.sessionState.intent.name === "getrecs" ||
+          parsed.sessionState.intent.name === "moviesbydirector" ||
+          parsed.sessionState.intent.name === "moviesbyactor");
+
       const index = messages.findIndex((msg) => {
         return msg.content === "..." && msg.type === "bot";
       });
       const msgCopy = [...messages];
 
+      if (intentFinished) {
+        getrecsSetMessages(
+          index,
+          msgCopy,
+          parsed,
+          messages,
+          parsed.sessionState.intent.name
+        );
+      } else {
+        msgCopy[index] = {
+          ...messages[index],
+          content: parsed.messages.content,
+          card: false,
+        };
+      }
+
+      /*
       let newContent =
         "Sorry, but we did not find any movie results that matched your query";
       let cards = [];
@@ -65,15 +122,12 @@ const getMessage = (
           isCard = true;
         }
       }
-      msgCopy[index] = {
-        ...messages[index],
-        content: newContent,
-        card: isCard,
-      };
+      */
 
       /* after these, should push another bot message that asks user if there is anything
       else they would like to do */
 
+      /*
       if (getrecsFinished) {
         for (const movieCard of cards.slice(1)) {
           msgCopy.push({
@@ -87,10 +141,11 @@ const getMessage = (
           card: false,
         });
       }
+      */
       console.log("setting new messages... ", msgCopy);
       setMessages(msgCopy);
 
-      // if we have fulfilled getrecs (or anything...), then reset
+      // if we have fulfilled getrecs (or anything...), then reset metadata
       if (parsed.sessionState.intent.state === "Fulfilled") {
         console.log("fulfilled! reset");
         setMetadata({
