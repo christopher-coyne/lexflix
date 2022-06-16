b3197583ae731a9d3b605634007bcfd696ec1110
christopher coyne <christcoyne@gmail.com>
2022-06-16 19:21:47 -0400

    got rid of h2 inside of p error

--- src/components/Logs/BotMessage/components/BotLogo/BotLogo.js
+++ src/components/Logs/BotMessage/components/BotLogo/BotLogo.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const BotLogo = () => {
   return (
-    <h2 className="text-yellow font-lobster absolute -left-7 -top-5 border-yellow border bg-midnightPurple rounded-full h-10 w-10 py-1 pr-1 z-[1] text-center ">
+    <span className="text-yellow font-lobster absolute -left-2 sm:-left-7 -top-6 border-yellow border bg-midnightPurple rounded-full h-10 w-10 py-1 pr-1 z-[1] text-center ">
       L
-    </h2>
+    </span>
   );
 };
 
