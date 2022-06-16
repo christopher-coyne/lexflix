4943a74cdf2e222489e6e51321a2cd0fcd78cb3e
christopher coyne <christcoyne@gmail.com>
2022-06-16 19:22:41 -0400

    extension max height added, scroll

--- src/components/Suggestions/Extension/Extension.js
+++ src/components/Suggestions/Extension/Extension.js
@@ -3,7 +3,7 @@ import Icon from "../Icon/Icon";
 
 const Extension = ({ currentExtension, submit }) => {
   return (
-    <div className="h-[500] w-[40%] border-2 border-pink absolute bottom-[10%] right-[27%] bg-midnightPurple text-left p-4 rounded-lg">
+    <div className="h-[500] w-[70%] sm:w-[70%] md:w-[60%] lg:w-[50%] border-2 border-pink absolute bottom-[10%] right-[27%] bg-midnightPurple text-left p-4 rounded-lg z-40 max-h-[20rem] overflow-y-scroll">
       {currentExtension.map((sug) => (
         <Icon submit={submit} text={sug} key={sug} />
       ))}
