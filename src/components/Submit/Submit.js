9dcd5d6722436fd93c1ff5626a04398cdebbd546
christopher coyne <christcoyne@gmail.com>
2022-06-16 19:23:05 -0400

    submit padding/styling changes

--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -4,12 +4,12 @@ import Suggestions from "../../components/Suggestions/Suggestions";
 
 const Submit = ({ userInput, updateInput, submitHandler, metadata }) => {
   return (
-    <div className="w-11/12 border-2 border-fontLightBlue xl:w-7/12 lg:w-8/12 md:w-9/12 sm:w-10/12 text-right">
+    <div className="w-11/12 xl:w-7/12 lg:w-8/12 md:w-9/12 sm:w-10/12 text-right border-2 border-yellow h-[40%] xs:h-[30%] lg:h-[20%] md:border-white flex flex-col justify-end pb-5 max-w-screen-lg">
       <Suggestions metadata={metadata} submit={submitHandler} />
       <form
         onSubmit={(e) => submitHandler(e)}
         autoComplete="off"
-        className=" w-full h-12 border-4 border-white flex flex-between md:flex-none"
+        className=" w-full h-12 flex flex-between md:flex-none"
       >
         <input
           type="text"
