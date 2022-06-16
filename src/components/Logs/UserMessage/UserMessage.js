c48327ff99498ff9038583e7229282cbe1334b53
christopher coyne <christcoyne@gmail.com>
2022-06-16 19:23:30 -0400

    width adjusted on small screens for user messages

--- src/components/Logs/UserMessage/UserMessage.js
+++ src/components/Logs/UserMessage/UserMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserMessage = ({ message }) => {
   return (
     <div className="text-right mt-2 font-oxygen">
-      <p className="bg-pink text-white px-4 py-3 max-w-[50%] rounded-bl-2xl rounded-tl-2xl rounded-br-2xl rounded-tr-sm ml-auto mr-0 inline-block text-left transition-all">
+      <p className="bg-pink text-white px-4 py-3 rounded-bl-2xl rounded-tl-2xl rounded-br-2xl rounded-tr-sm ml-auto mr-0 inline-block text-left transition-all max-w-full xs:max-w-[%85] sm:max-w-[75%] md:max-w-[60%] lg:max-w-[50%]">
         {message}
       </p>
     </div>
