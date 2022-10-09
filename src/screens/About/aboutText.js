fff83c894f6f148f166609fa453d09aba854f5e0
christopher coyne <christcoyne@gmail.com>
2022-10-09 15:11:58 -0400

    imdb bot aws architecture image

--- src/screens/About/aboutText.js
+++ src/screens/About/aboutText.js
@@ -1,7 +1,6 @@
 const aboutText = {
   stack: `React + Tailwind CSS on the frontend, AWS on the backend. Specifically, API Gateway to handle incoming requests,
-    Lambda functions (in python + boto3) to handle the Lex chatbot and fetch from the database, and Dynamodb. Cloudformation
-    and Codepipeline were used for improved CICD.`,
+    Lambda functions (in python + boto3) to handle the Lex chatbot and fetch from the database in DynamoDB.`,
   data: `Of course. The JSON data is available on the project's github, located`,
 };
 
