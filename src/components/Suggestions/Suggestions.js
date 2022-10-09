86de48471bc98ef87aacbefd8ba9272775b946d0
christopher coyne <christcoyne@gmail.com>
2022-10-09 15:13:15 -0400

    removed unused import

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import Icon from "./Icon/Icon";
 import Extension from "./Extension/Extension";
 import { suggestionsContext } from "../../contexts/suggestionsContext";
